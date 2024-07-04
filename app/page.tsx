5567cd446eab4fb68d204bce89cd63695cec74c4
youngboypimpin <rjtekdom@gmail.com>
2024-07-04 16:24:54 +0100

    added cdnsanityio, updated design for projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+
+
 import { getProjects } from "@/sanity/sanity-utils";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
 	try {
@@ -24,22 +27,23 @@ export default async function Home() {
 
 				<div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8" >
 				{projects.map((project) => (
-					<div
+					<Link
+					href={`/projects/${project.slug}`}
 						key={project._id}
-						className="border border-gray-500 rounded-lg p-3">
+						className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition">
 						{project.image &&(
 							<Image
 							src = {project.image}
 							alt = {project.name}
-							width = {250}
-							height = {100}
+							width = {750}
+							height = {300}
 							className = 'object-cover rounded-lg border border-gray-500'
 							/>
 						)}	
 						<div className="font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
 							{project.name}
 						</div>
-					</div>
+					</Link>
 				))}
 				</div>
 				
