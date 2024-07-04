c45902aaebe61f64d9bc5cf539717b56eec6ce27
youngboypimpin <rjtekdom@gmail.com>
2024-07-04 17:01:54 +0100

    feat: project page works

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -23,3 +23,31 @@ export async function getProjects(): Promise<Project[]> {
         }`
 	);
 }
+
+
+export async function getProject(slug: String): Promise<Project>{
+
+    const client = createClient({
+		projectId: "d8emt0pr",
+
+		dataset: "production",
+
+		apiVersion: "2024-06-30",
+	});
+
+	return client.fetch(
+		groq`*[_type == 'project' && slug.current == $slug][0]{
+
+            _id,
+            _createdAt,
+            name,
+            'slug': slug.current,
+            'image': image.asset->url,
+            url,
+            content,
+        }`,
+        {slug}
+	);
+
+
+}
