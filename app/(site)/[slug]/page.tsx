5b9b43c16ce696284f7449e43959dfb9e8b015ca
youngboypimpin <rjtekdom@gmail.com>
2024-08-02 16:48:08 +0100

    added loading state for page and conditional elements based on page-slug

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -1,22 +1,45 @@
-// sanitytester\app\(site)\[slug]\page.tsx
+"use client"
 
 import { getPage } from "@/sanity/sanity-utils";
 import { PortableText } from "next-sanity";
+import { useEffect, useState } from "react"; // Import React hooks for async rendering
 
-type Prop = {
-	params: { slug: string }
-}
+type PageProps = {
+  params: { slug: string };
+};
 
-export default async function Page({ params }: Prop) {
-	const page = await getPage(params.slug);
+export default function Page({ params }: PageProps) {
+  const [page, setPage] = useState<any>(null);
 
-	return (
-		<div className="w-full md:w-[75%] mx-auto">
-			<h1 className="bg-gradient-to-r from-green-900 via-green-700 to-green-300 bg-clip-text text-transparent text-5xl drop-shadow font-extrabold" >{page.title}</h1>
+  useEffect(() => {
+    const fetchPage = async () => {
+      const fetchedPage = await getPage(params.slug);
+      setPage(fetchedPage);
+    };
 
-			<div className="prose prose-a:underline prose-a:text-white hover:prose-a:text-gray-400 transition text-lg text-white mt-10" >
-				<PortableText value={page.content} />
-			</div>
-		</div>
-	)
+    fetchPage();
+  }, [params.slug]);
+
+  if (!page) {
+    return <div className="text-white" >Loading...</div>; // Placeholder for loading state
+  }
+
+  return (
+    <div className="w-full md:w-[75%] mx-auto">
+      <h1 className="my-14 text-white text-5xl font-bold">{page.title}</h1>
+
+      <div className="prose prose-a:underline prose-a:text-white hover:prose-a:text-gray-400 transition text-lg text-white mt-10">
+        <PortableText value={page.content} />
+      </div>
+
+      {page.slug === "contact" && (
+        <div className="text-white mt-10">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur,
+          sunt? Fuga quia atque dolor dolores quibusdam id, adipisci eum quidem
+          ullam laborum. Repellat, facere animi excepturi unde nulla perferendis
+          id.
+        </div>
+      )}
+    </div>
+  );
 }
