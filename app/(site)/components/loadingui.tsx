66f8f4b783375ce8807657ff2585d1cb2a8ba62e
youngboypimpin <rjtekdom@gmail.com>
2024-08-03 20:21:15 +0100

    I centered a div!

--- app/(site)/components/loadingui.tsx
+++ app/(site)/components/loadingui.tsx
@@ -3,7 +3,7 @@
 export default function LoadingUI() {
 
 	return (
-		<div className=" flex justify-center">
+		<div className="100vh flex justify-center">
 			<span className=" text-9xl font-extrabold bg-gradient-to-r from-green-900 via-green-700 to-green-300 bg-clip-text text-transparent animate-pulse">
 				Oti.
 			</span>
