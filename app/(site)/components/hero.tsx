55a2eefd3ccb6315fdf97e546eeafdb84fe93b19
youngboypimpin <rjtekdom@gmail.com>
2024-08-03 22:23:12 +0100

    created hero component

---
+++ app/(site)/components/hero.tsx
@@ -0,0 +1,24 @@
+
+
+
+export default function Hero() {
+
+	return (
+		<>
+			<h1 className="text-7xl font-extrabold text-white">
+				Hello I&apos;m{" "}
+				<span className="bg-gradient-to-r from-green-900 via-green-700 to-green-300 bg-clip-text text-transparent">
+					Oti.
+				</span>
+			</h1>
+			<p className="mt-3 text-xl text-white">
+				Welcome to my portfolio!
+				<br /><i><sub>displayed below are my most confident works</sub></i>
+			</p>
+			<h2 className="mt-24 font-bold text-white text-3xl">
+				My Projects
+			</h2>
+		</>
+
+	)
+}
