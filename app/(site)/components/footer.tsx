93c8dea7be60c29e836f7607d73e3275cf6673d9
youngboypimpin <rjtekdom@gmail.com>
2024-08-04 00:00:58 +0100

    modified footer for responsiveness

--- app/(site)/components/footer.tsx
+++ app/(site)/components/footer.tsx
@@ -5,22 +5,26 @@ export default function Footer() {
 
 	return (
 
-		<footer className=" text-sm lg:text-base bg-gray-950 bg-opacity-15 backdrop-blur-md text-white flex flex-row justify-center border-t-2 border-green-500 fixed inset-x-0 bottom-0 p-4 gap-4">
-			<p>&copy; Oti. Built by Patrick Aigbogun</p>
-			<div className="flex items-center gap-2 hover:scale-125 transition">
-				<Phone size={24} weight="duotone" />
-				<span>+12345678</span>
+		<footer className="  max-h-[18%] p-1 text-sm lg:text-base bg-gray-950 bg-opacity-15 backdrop-blur-md text-white border-t-2 border-green-500 fixed inset-x-0 bottom-0 ">
+			<div className=" flex flex-col sm:flex-row sm:justify-center gap-x-4 items-center max-w-[90%] my-2 space-y-2">
+				<div className="inline-block" ><p>&copy; Oti. Built by Patrick Aigbogun</p> </div>
+				<div className="inline-flex flex-row gap-4 " >
+				<span className="flex flex-row hover:scale-125 transition">
+					<Phone size={24} weight="duotone" />
+					<span>+12345678</span>
+				</span>
+				{/* <div className="flex items-center gap-2">
+							<EnvelopeSimple size={24} weight="duotone" />
+							<span>name@email.com</span>
+						</div> */}
+				<Link href={"https://linkedin.com/username"} className=" hover:scale-125 transition">
+					<LinkedinLogo size={24} weight="duotone" />
+				</Link>
+				<Link href={"https://github.com/username"} className=" hover:scale-125 transition">
+					<GithubLogo size={24} weight="duotone" />
+				</Link>
+				</div>
 			</div>
-			{/* <div className="flex items-center gap-2">
-						<EnvelopeSimple size={24} weight="duotone" />
-						<span>name@email.com</span>
-					</div> */}
-			<Link href={"https://linkedin.com/username"} className="flex items-center gap-2 hover:scale-125 transition">
-				<LinkedinLogo size={24} weight="duotone" />
-			</Link>
-			<Link href={"https://github.com/username"} className="flex items-center gap-2 hover:scale-125 transition">
-				<GithubLogo size={24} weight="duotone" />
-			</Link>
 		</footer>
 
 	)
