9239da34b98c1b1d155d437a727bdc9258ac7f9e
youngboypimpin <rjtekdom@gmail.com>
2024-08-04 00:12:09 +0100

    modified for UI consistency... might be changed

--- app/(site)/components/header.tsx
+++ app/(site)/components/header.tsx
@@ -20,12 +20,12 @@ export default async function Header() {
 						className="bg-gradient-to-r from-green-900 via-green-700 to-green-300 bg-clip-text text-transparent font-bold hover:scale-125 transition">
 						Oti.
 					</Link>
-					<div className="flex justify-between gap-3 text-gray-200">
+					<div className="flex text-xl justify-between gap-3 text-gray-200">
 						{pages.map((page) => (
 							<Link
 								href={`/${page.slug}`}
 								key={page._id}
-								className="hover:underline hover:scale-110 hover:text-white hover:font-medium transition">
+								className=" bg-white rounded-lg text-black font-medium sm:font-medium p-2 whitespace-nowrap hover:font-bold hover:underline hover:scale-110 transition">
 								{page.title}
 							</Link>
 						))}
