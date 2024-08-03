1e0fd2438c5f816521ec9a307d3ae5be2330d57c
youngboypimpin <rjtekdom@gmail.com>
2024-08-03 21:15:12 +0100

    added a nav menu

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -8,44 +8,48 @@ import ContactForm from "../components/contactform";
 import Link from "next/link";
 
 type PageProps = {
-  params: { slug: string };
+	params: { slug: string };
 };
 
 export default function Page({ params }: PageProps) {
-  const [page, setPage] = useState<any>(null);
+	const [page, setPage] = useState<any>(null);
 
-  useEffect(() => {
-    const fetchPage = async () => {
-      const fetchedPage = await getPage(params.slug);
-      setPage(fetchedPage);
-    };
+	useEffect(() => {
+		const fetchPage = async () => {
+			const fetchedPage = await getPage(params.slug);
+			setPage(fetchedPage);
+		};
 
-    fetchPage();
-  }, [params.slug]);
+		fetchPage();
+	}, [params.slug]);
 
-  if (!page) {
-    return <LoadingUI/>; // Placeholder for loading state
-  }
+	if (!page) {
+		return <LoadingUI />; // Placeholder for loading state
+	}
 
-  return (
-    <div className="mb-10 w-full md:w-[75%] mx-auto">
-		<header className=" my-2 p-0 flex items-center justify-between gap-2" >
+	return (
+		<div className="mb-10 w-full md:w-[75%] mx-auto">
+			<header className=" my-2 p-0 flex items-center justify-between gap-2" >
 				<Link href="../" title="Home" rel="noopener noreferrer" className=" m-0 bg-white rounded-lg text-black font-bold sm:font-medium p-3 whitespace-nowrap hover:bg-gray-800 hover:text-white transition">
 					Home
 				</Link>
-				<Link href="" title="View project" target="_blank" rel="noopener noreferrer" className=" m-0 bg-white rounded-lg text-black font-bold sm:font-medium p-3 whitespace-nowrap hover:bg-gray-800 hover:text-white transition" >
+				<button
+					onClick={() => window.history.back()}
+					title="Go back"
+					className="m-0 bg-white rounded-lg text-black font-bold sm:font-medium p-3 whitespace-nowrap hover:bg-gray-800 hover:text-white transition"
+				>
 					Go back
-				</Link>
+				</button>
 			</header>
-      <h1 className="my-14 text-white text-5xl font-bold">{page.title}</h1>
+			<h1 className="my-14 text-white text-5xl font-bold">{page.title}</h1>
 
-      <div className="prose prose-a:underline prose-a:text-white hover:prose-a:text-gray-400 transition text-lg text-white mt-10">
-        <PortableText value={page.content} />
-      </div>
+			<div className="prose prose-a:underline prose-a:text-white hover:prose-a:text-gray-400 transition text-lg text-white mt-10">
+				<PortableText value={page.content} />
+			</div>
 
-      {page.slug === "contact" && (
-        <ContactForm/>
-      )}
-    </div>
-  );
+			{page.slug === "contact" && (
+				<ContactForm />
+			)}
+		</div>
+	);
 }
