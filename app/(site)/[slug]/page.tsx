38f48314ef488c4e719cdc958a7e92643a8a7d78
youngboypimpin <rjtekdom@gmail.com>
2024-08-03 21:07:02 +0100

    minor edits

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { PortableText } from "next-sanity";
 import { useEffect, useState } from "react"; // Import React hooks for async rendering
 import LoadingUI from "../components/loadingui";
 import ContactForm from "../components/contactform";
+import Link from "next/link";
 
 type PageProps = {
   params: { slug: string };
@@ -28,6 +29,14 @@ export default function Page({ params }: PageProps) {
 
   return (
     <div className="mb-10 w-full md:w-[75%] mx-auto">
+		<header className=" my-2 p-0 flex items-center justify-between gap-2" >
+				<Link href="../" title="Home" rel="noopener noreferrer" className=" m-0 bg-white rounded-lg text-black font-bold sm:font-medium p-3 whitespace-nowrap hover:bg-gray-800 hover:text-white transition">
+					Home
+				</Link>
+				<Link href="./" title="View project" target="_blank" rel="noopener noreferrer" className=" m-0 bg-white rounded-lg text-black font-bold sm:font-medium p-3 whitespace-nowrap hover:bg-gray-800 hover:text-white transition" >
+					Go back
+				</Link>
+			</header>
       <h1 className="my-14 text-white text-5xl font-bold">{page.title}</h1>
 
       <div className="prose prose-a:underline prose-a:text-white hover:prose-a:text-gray-400 transition text-lg text-white mt-10">
