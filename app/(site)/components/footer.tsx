0d798c240197dc1a45115b45cee26c58660e0e14
youngboypimpin <rjtekdom@gmail.com>
2024-08-04 00:46:23 +0100

    minor changes

--- app/(site)/components/footer.tsx
+++ app/(site)/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
 
 	return (
 
-		<footer className="  max-h-[18%] p-1 text-sm lg:text-base bg-gray-950 bg-opacity-15 backdrop-blur-md text-white border-t-2 border-green-500 fixed inset-x-0 bottom-0 ">
+		<footer className="  max-h-[18%] p-1 font-medium text-sm lg:text-base bg-gray-950 bg-opacity-15 backdrop-blur-md text-white border-t-2 border-green-500 fixed inset-x-0 bottom-0 ">
 			<div className=" flex flex-col sm:flex-row sm:justify-center gap-x-4 items-center max-w-[90%] my-2 space-y-2">
 				<div className="inline-block" ><p>&copy; Oti. Built by Patrick Aigbogun</p> </div>
 				<div className="inline-flex flex-row gap-4 " >
