774ae18c016d2e4a90585bba9becee7da9819ab5
youngboypimpin <rjtekdom@gmail.com>
2024-08-03 15:49:42 +0100

    created a loadingUI for delayed fetching

---
+++ app/(site)/components/loadingui.tsx
@@ -0,0 +1,12 @@
+
+
+export default function LoadingUI() {
+
+	return (
+		<div className=" flex justify-center">
+			<span className=" text-9xl font-extrabold bg-gradient-to-r from-green-900 via-green-700 to-green-300 bg-clip-text text-transparent animate-pulse">
+				Oti.
+			</span>
+		</div>
+	)
+}
