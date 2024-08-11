f6a80161bb9a08e668654190e644161e1c74bd85
youngboypimpin <rjtekdom@gmail.com>
2024-08-12 00:46:45 +0100

    feat: accent colour to menu links

--- app/(site)/components/header.tsx
+++ app/(site)/components/header.tsx
@@ -17,7 +17,7 @@ export default async function Header() {
         <header className={` ${montserrat.className} text-3xl flex items-center justify-between`} >
 					<Link
 						href="/"
-						className="bg-gradient-to-r from-green-900 via-green-700 to-green-300 bg-clip-text text-transparent font-bold hover:scale-125 transition">
+						className="bg-gradient-to-r from-green-900 via-green-700 to-green-300 bg-clip-text text-transparent font-bold hover:scale-125 transition ease-linear">
 						Oti.
 					</Link>
 					<div className="flex text-xl justify-between gap-3 text-gray-200">
@@ -25,7 +25,7 @@ export default async function Header() {
 							<Link
 								href={`/${page.slug}`}
 								key={page._id}
-								className=" text-white font-medium sm:font-medium p-2 hover:font-bold hover:underline hover:scale-110 transition">
+								className=" text-white font-medium sm:font-medium p-2 hover:font-bold hover:underline hover:decoration-green-500 hover:scale-110 transition ease-linear">
 								{page.title}
 							</Link>
 						))}
