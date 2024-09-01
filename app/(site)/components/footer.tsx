5ac6fd76f2d8fd2834314af7a9ad8acb5f517946
youngboypimpin <rjtekdom@gmail.com>
2024-09-01 15:03:44 +0100

    green bg footer

--- app/(site)/components/footer.tsx
+++ app/(site)/components/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
 
 	return (
 
-		<footer className={` ${montserrat.className} flex justify-center max-h-[18%] p-1 font-medium text-sm lg:text-base bg-gray-950 bg-opacity-15 backdrop-blur-lg text-white border-t fixed inset-x-0 bottom-0 `}>
+		<footer className={` ${montserrat.className} flex justify-center max-h-[18%] p-1 font-medium text-sm lg:text-base bg-green-800 bg-opacity-15 backdrop-blur-lg text-white border-t-green-500 fixed inset-x-0 bottom-0 `}>
 			<div className=" flex flex-col sm:flex-row sm:justify-center gap-x-4 items-center max-w-[90%] my-2 space-y-2">
 				<div className="inline-block" ><p>&copy; Oti. Built by Patrick Aigbogun</p> </div>
 				<div className="inline-flex flex-row gap-4 " >
