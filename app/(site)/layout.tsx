5da9c50fa4ce1c699bc47d847266a83dbc4d91a7
youngboypimpin <rjtekdom@gmail.com>
2024-09-01 15:03:12 +0100

    darkened scrollbar

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -20,7 +20,7 @@ export default async function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html className="overflow-y-scroll scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-gray-700/20 scrollbar-thumb-green-500" lang="en">
+		<html className="overflow-y-scroll scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-gray-700/20 scrollbar-thumb-green-900/60" lang="en">
 			<body className={`bg-black mx-auto p-5 sm:p-20 ${nunito.className} `}>
 				<HeadWrapper />
 				<main className="py-20">
