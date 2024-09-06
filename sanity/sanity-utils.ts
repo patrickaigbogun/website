da425c41f7a783ac8a35e98ad15b42bed73ff3b6
youngboypimpin <rjtekdom@gmail.com>
2024-09-06 22:56:02 +0100

    commented out author fetch as it is not needed

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -6,6 +6,9 @@ import clientConfig from "./config/client-config";
 import { Page } from "@/types/Page";
 import { blogPost } from "@/types/blogPost";
 
+// UNCOMMENT WHEN YOU UNCOMMENT AUTHOR ASYNC FETCHES
+// import { Author } from "@/types/author"
+
 export async function getProjects(): Promise<Project[]> {
 
 
@@ -29,13 +32,13 @@ export async function getProject(slug: String): Promise<Project> {
 	return createClient(clientConfig).fetch(
 		groq`*[_type == 'project' && slug.current == $slug][0]{
 
-            _id,
-            _createdAt,
-            name,
-            'slug': slug.current,
-            'image': image.asset->url,
-            url,
-            content
+			_id,
+			_createdAt,
+			name,
+			'slug': slug.current,
+			'image': image.asset->url,
+			url,
+			content
         }`,
 		{ slug }
 	);
@@ -48,10 +51,10 @@ export async function getPages(): Promise<Page[]> {
 
 	return createClient(clientConfig).fetch(
 		groq`*[_type == 'page']{
-          _id,
-          _createdAt,
-          title,
-          'slug': slug.current
+			_id,
+			_createdAt,
+			title,
+			'slug': slug.current
         }`
 	);
 }
@@ -60,11 +63,11 @@ export async function getPage(slug: String): Promise<Page> {
 
 	return createClient(clientConfig).fetch(
 		groq`*[_type == 'page' && slug.current == $slug][0]{
-            _id,
-          _createdAt,
-          title,
-          'slug': slug.current,
-          content
+			_id,
+			_createdAt,
+			title,
+			'slug': slug.current,
+			content
         }`,
 		{ slug }
 	);
@@ -90,17 +93,57 @@ export async function getblogPosts(): Promise<blogPost[]> {
 export async function getblogPost(slug: String): Promise<blogPost> {
 
 	return createClient(clientConfig).fetch(
-		groq`*[_type == 'blogPost']{
+		groq`*[_type == 'blogPost' && slug.current == $slug][0]{
 
-    _id,
-    _createdAt,
-    name,
-    'slug': slug.current,
-    'image': image.asset->url,
-    url,
-    content
-}`,{slug}
+			_id,
+			_createdAt,
+			title,
+			'slug': slug.current,
+			tagline,
+			excerpt,
+			publishDate,
+			authors,
+			'image': image.asset->url,
+			content
+		}`,
+		{ slug }
 	);
 
 
 }
+
+// UNCOMMENT IF YOU BUILD AN INDIVIDUAL AUTHOR PAGE OR NEED TO FETCH AUTHORS FOR SOME REASON
+
+
+// export async function getAuthors(slug: String): Promise<Page> {
+
+// 	return createClient(clientConfig).fetch(
+// 		groq`*[_type == 'page']{
+// 			_id,
+// 			_createdAt,
+// 			name,
+// 			'image': image.asset->url,
+			
+//         }`
+// 	);
+// }
+
+
+// export async function getAuthor(slug: String): Promise<Author> {
+
+// 	return createClient(clientConfig).fetch(
+// 		groq`*[_type == 'author' && slug.current == $slug][0]{
+
+// 			_id,
+// 			_createdAt,
+// 			name,
+// 			'slug': slug.current,
+// 			publishDate,
+// 			'image': image.asset->url,
+
+// 		}`, 
+// 		{ slug }
+// 	);
+
+
+// }
