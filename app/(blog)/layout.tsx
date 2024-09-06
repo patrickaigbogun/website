db1c2cd74557761a2b90a8a464cee8ed1f3b1718
youngboypimpin <rjtekdom@gmail.com>
2024-09-06 23:03:38 +0100

    building the blog

---
+++ app/(blog)/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import { nunito } from "@/fonts/fonts";
+
+
+export const metadata: Metadata = {
+	title: "Patrick Aigbogun, Oti. Blog",
+	description: "Personal blog where i discuss technologies, philosophy and poetry",
+};
+
+export default async function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html className="overflow-y-scroll scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-gray-700/20 scrollbar-thumb-green-900/60" lang="en">
+			<body className={`bg-black mx-auto p-5 sm:p-20 ${nunito.className} `}>
+				<main className="m-5">
+					{children}
+				</main>
+			</body>
+		</html>
+	);
+}
--- app/(blog)/page.tsx
+++ app/(blog)/page.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
-function page() {
+export default function BlogHome() {
   return (
     <div>page</div>
   )
 }
-
-export default page
