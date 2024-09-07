7e44372eca39378ad6ac45a06b3ba4e8e59424d4
youngboypimpin <rjtekdom@gmail.com>
2024-09-07 04:09:59 +0100

    minor changes

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -15,7 +15,7 @@ export default async function RootLayout({
 }>) {
 	return (
 		<html className="overflow-y-scroll scrollbar-thin scrollbar-thumb-rounded-full scrollbar-track-gray-700/20 scrollbar-thumb-green-900/60" lang="en">
-			<body className={`bg-black mx-auto p-5 sm:p-20 ${nunito.className} `}>
+			<body className={`bg-indigo-950 htext-gray-600 mx-auto p-5 sm:p-20 ${nunito.className} `}>
 				<main className="m-5">
 					{children}
 				</main>
